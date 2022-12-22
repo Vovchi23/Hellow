31394e82a5576f1c20c0a4f448a2bf644f8126ff
Vivchik <vovka.azik@mail.ru>
2022-12-22 18:05:55 +0300

    zadacha

--- Example_007/Program.cs
+++ Example_007/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.Clear();
-Console.SetCursorPosition(10, 4);
-Console.WriteLine("+");
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
 
 int xa = 1;
 int ya = 1;
 int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int yb = 20;
+int xc = 30;
+int yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
